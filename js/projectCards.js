5ce59386a8decb2fa7a20ff73871357c85b0c2c5
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-07-14 16:02:11 -0400

    creating js component using bootstrap classes

--- js/projectCards.js
+++ js/projectCards.js
@@ -1,6 +1,9 @@
 const projectInfo = [
   {
-    name: "DryMaster Restoration"
+    img: "../imgs/umts-screen.png",
+    name: "Use My Tech Stuff",
+    languages: "HTML, CSS, JS",
+    info: ""
   }
 ];
 
@@ -14,12 +17,36 @@ projectInfo.forEach(data => {
 function projectComp(data) {
   //elements and classList
   const projectCard = document.createElement("div");
-  projectCard.classList.add("projectCard");
+  projectCard.classList.add("card");
 
-  const proName = document.createElement("h5");
-  proName.textContent = data.name;
+  const cardImg = document.createElement("img");
+  cardImg.classList.add("card-img-top");
+  cardImg.src = data.img;
 
-  projectCard.appendChild(proName);
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+
+  const cardTitle = document.createElement("h5");
+  cardTitle.textContent = data.name;
+
+  const cardInfo = document.createElement("p");
+  cardInfo.classList.add("card-text");
+  cardInfo.textContent = data.info;
+
+  projectCard.appendChild(cardImg);
+  projectCard.appendChild(cardBody);
+  cardBody.appendChild(cardTitle);
 
   return projectCard;
+}
+
+{
+  /* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */
 }
